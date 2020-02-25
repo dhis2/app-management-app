18d2154d24be90cf66d0a3caad6825e0c3fe2602
Viktor Varland <viktor@dhis2.org>
2020-02-25 10:04:36 +0100

    feat(app-hub): use the app-hub directly (#88)
    * feat: migrate to app hub
    
    * feat(app-hub): use the app hub
    
    * fix(d2): work around d2 having appHub hardcoded

--- src/actions.js
+++ src/actions.js
@@ -4,8 +4,8 @@ import Action from 'd2-ui/lib/action/Action';
 import { getInstance as getD2 } from 'd2/lib/d2';
 import log from 'loglevel';
 
-import appStoreStore from './stores/appStore.store';
-import installedAppStore from './stores/installedApp.store';
+import appHubStore from './stores/appHub.store';
+import installedAppHub from './stores/installedApp.store';
 
 import i18n from '@dhis2/d2-i18n'
 
@@ -19,8 +19,8 @@ const actions = {
     appInstalled: Action.create('An app was installed'),
 
     // App store actions
-    loadAppStore: Action.create('Load DHIS2 App Store'),
-    installAppVersion: Action.create('Install App Version from DHIS2 App Store'),
+    loadAppHub: Action.create('Load DHIS2 App Hub'),
+    installAppVersion: Action.create('Install App Version from DHIS2 App Hub'),
 
     // Snackbar
     showSnackbarMessage: Action.create('Show Snackbar message'),
@@ -37,7 +37,7 @@ actions.installApp.subscribe((params) => {
         d2.system.uploadApp(zipFile, progressCallback)
             .then(() => d2.system.reloadApps())
             .then((apps) => {
-                installedAppStore.setState(apps);
+                installedAppHub.setState(apps);
 
                 actions.showSnackbarMessage(i18n.t('App installed successfully'));
                 actions.appInstalled(zipFile.name.substring(0, zipFile.name.lastIndexOf('.')));
@@ -77,16 +77,16 @@ actions.uninstallApp.subscribe((params) => {
 actions.refreshApps.subscribe(() => {
     getD2().then((d2) => {
         d2.system.reloadApps().then((apps) => {
-            installedAppStore.setState(apps);
+            installedAppHub.setState(apps);
         });
     });
 });
 
 
 /*
- * Load the app store
+ * Load the app hub
  */
-actions.loadAppStore.subscribe(async () => {
+actions.loadAppHub.subscribe(async () => {
 
     const d2 = await getD2();
     const baseUrl = d2.Api.getApi().baseUrl;
@@ -99,10 +99,10 @@ actions.loadAppStore.subscribe(async () => {
         }
     }
     
-    const getAppstoreUrl = async () => {    
-        const response = await fetch(`${baseUrl}/configuration/settings/filter.json?type=CONFIGURATION`, fetchOptions)
+    const getAppHubUrl = async () => {    
+        const response = await fetch(`${baseUrl}/configuration/appHubUrl`, fetchOptions)
         const dhis2Configuration = await response.json();
-        return dhis2Configuration['dhis-configurations']['appstore.api.url'];
+        return dhis2Configuration.apiUrl;
     }
     
     const getDhisVersion = async () => {
@@ -112,8 +112,8 @@ actions.loadAppStore.subscribe(async () => {
         return json.version.replace('-SNAPSHOT', '');
     }
 
-    const url = await getAppstoreUrl();
-    debug(`Got appstore url: ${url}`)
+    const url = await getAppHubUrl();
+    debug(`Got apphub url: ${url}`)
     
     const version = await getDhisVersion();
     debug(`Got dhis2 version: ${version}`)
@@ -123,41 +123,52 @@ actions.loadAppStore.subscribe(async () => {
 
     const response = await fetch(`${url}/apps?dhis_version=${version}`, corsOptions)
     const apps = await response.json();
-    appStoreStore.setState(Object.assign(appStoreStore.getState() || {}, { apps }));
+    appHubStore.setState(Object.assign(appHubStore.getState() || {}, { apps }));
 });
 
 
 /*
- * Install app version from the app store
+ * Install app version from the app hub
  */
 actions.installAppVersion.subscribe((params) => {
     const versionId = params.data[0];
-    const appStoreState = appStoreStore.getState();
-    appStoreStore.setState(Object.assign(appStoreState, {
-        installing: appStoreState.installing ? appStoreState.installing + 1 : 1,
+    const appHubState = appHubStore.getState();
+    appHubStore.setState(Object.assign(appHubState, {
+        installing: appHubState.installing ? appHubState.installing + 1 : 1,
     }));
 
     getD2().then((d2) => {
-        actions.showSnackbarMessage(i18n.t('Installing app from the app store...'));
-        d2.system.installAppVersion(versionId)
+        actions.showSnackbarMessage(i18n.t('Installing app from the app hub...'));
+        installAppVersion(versionId, d2)
             .then(() => d2.system.reloadApps())
             .then((apps) => {
                 actions.showSnackbarMessage(i18n.t('App installed successfully'));
-                const appStoreState2 = appStoreStore.getState();
-                appStoreStore.setState(Object.assign(appStoreState2, { installing: appStoreState2.installing - 1 }));
-                installedAppStore.setState(apps);
+                const appHubState2 = appHubStore.getState();
+                appHubStore.setState(Object.assign(appHubState2, { installing: appHubState2.installing - 1 }));
+                installedAppHub.setState(apps);
                 params.complete(apps);
             })
             .catch((err) => {
                 actions.showSnackbarMessage(
-                    `${i18n.t('Failed to install an app from the app store')}: ${err.message}`,
+                    `${i18n.t('Failed to install an app from the app hub')}: ${err.message}`,
                 );
-                appStoreStore.setState(Object.assign(appStoreStore.getState(), {
-                    installing: appStoreStore.getState().installing - 1,
+                appHubStore.setState(Object.assign(appHubStore.getState(), {
+                    installing: appHubStore.getState().installing - 1,
                 }));
                 log.error(err);
             });
     });
 });
+
+function installAppVersion(uid, d2) {
+    const api = d2.Api.getApi();
+    return new Promise((resolve, reject) => {
+        api.post(['appHub', uid].join('/'), '', { dataType: 'text' }).then(() => {
+            resolve();
+        }).catch((err) => {
+            reject(err);
+        });
+    });
+}
 
 export default actions;
