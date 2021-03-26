17e05f628ff3a768bfb60be4040d4f1ffb579943
Médi-Rémi Hashim <mediremi@gmail.com>
2021-03-26 12:17:13 +0000

    chore: move TODO from code to review comment

--- src/get-latest-version.js
+++ src/get-latest-version.js
@@ -1,9 +1,5 @@
 import semver from 'semver'
 
-// TODO: Only return versions from stable channel?
-// Or take channel as param and get latest version for same channel as installed
-// app with stable being default
-
 export const getLatestVersion = versions =>
     versions.reduce((latestVersion, version) => {
         const parsedLatestVersion = semver.coerce(latestVersion.version)
