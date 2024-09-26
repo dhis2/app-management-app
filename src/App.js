97a53368238bdaed4e2de9043cffaea7768fa7b2
Joakim Storløkken Melseth <joakim.melseth@gmail.com>
2024-09-26 14:57:49 +0200

    fix: [DHIS2-17322] Ensure app hub id is in the right format

--- src/App.js
+++ src/App.js
@@ -12,6 +12,10 @@ import { InstalledApp } from './pages/InstalledApp/InstalledApp.js'
 import { ManualInstall } from './pages/ManualInstall/ManualInstall.js'
 import './locales/index.js'
 
+// Used to ensure we are matching the pattern of a valid app hub id, which should be a UUID version 4
+const APP_HUB_ID_REGEXP =
+    '[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}'
+
 const App = () => (
     <HashRouter>
         <QueryParamProvider
@@ -37,7 +41,10 @@ const App = () => (
                             component={InstalledApp}
                         />
                         <Route path="/app-hub" component={AppHub} />
-                        <Route path="/app/:appHubId" component={AppHubApp} />
+                        <Route
+                            path={`/app/:appHubId(${APP_HUB_ID_REGEXP})`}
+                            component={AppHubApp}
+                        />
                         <Route
                             path="/manual-install"
                             component={ManualInstall}
