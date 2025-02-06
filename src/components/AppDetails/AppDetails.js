728e17eb9c721457493189129d75f54fe6c867d9
Mozafar Haider <mozafar@dhis2.org>
2025-02-06 11:23:38 +0000

    chore: integrate iframe from apphub to show changelog

--- src/components/AppDetails/AppDetails.js
+++ src/components/AppDetails/AppDetails.js
@@ -2,7 +2,7 @@ import i18n from '@dhis2/d2-i18n'
 import { Button, Card, Divider } from '@dhis2/ui'
 import moment from 'moment'
 import PropTypes from 'prop-types'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { getAppIconSrc } from '../../get-app-icon-src.js'
 import { getLatestVersion } from '../../get-latest-version.js'
 import { AppIcon } from '../AppIcon/AppIcon.js'
@@ -10,6 +10,9 @@ import styles from './AppDetails.module.css'
 import { Description } from './Description.js'
 import { ManageInstalledVersion } from './ManageInstalledVersion.js'
 import { Versions } from './Versions.js'
+import { useApi } from '../../api.js'
+import { useAlert, useConfig } from '@dhis2/app-runtime'
+import semver from 'semver'
 
 const Metadata = ({ installedVersion, versions }) => {
     const relativeTime = (datetime) => moment(datetime).fromNow()
@@ -105,39 +108,104 @@ export const AppDetails = ({
         .map((i) => i.imageUrl)
     const versions = appHubApp?.versions.sort((a, b) => b.created - a.created)
 
+    const { installVersion, uninstallApp } = useApi()
+
+    const installSuccessAlert = useAlert(i18n.t('App installed successfully'), {
+        success: true,
+    })
+
+    const { serverVersion } = useConfig()
+
+    const dhisVersion = `${serverVersion.major}.${serverVersion.minor}`
+
+    console.log('???', serverVersion, dhisVersion)
+
+    const onInstall = async (version) => {
+        await installVersion(version)
+        installSuccessAlert.show()
+        onVersionInstall()
+    }
+
+    useEffect(() => {
+        window.onmessage = async (e) => {
+            if (e.data?.action === 'install') {
+                console.log(e.data)
+                // alert(JSON.stringify(e.data))
+                await onInstall(e.data?.version?.id)
+            }
+            // if (e.data == 'hello') {
+            //     alert('It works!')
+            // }
+        }
+    })
+
+    // const dhisVersion = '2.41'
+
     return (
         <Card className={styles.appCard}>
             <header className={styles.header}>
-                <div>
-                    <AppIcon src={logo} />
+                <div style={{ display: 'flex' }}>
+                    <div>
+                        <AppIcon src={logo} />
+                    </div>
+                    <div>
+                        <h1 className={styles.headerName}>{appName}</h1>
+                        {appDeveloper && (
+                            <span className={styles.headerDeveloper}>
+                                {i18n.t('by {{- developer}}', {
+                                    developer: appDeveloper,
+                                    context: 'developer of application',
+                                })}
+                            </span>
+                        )}
+                    </div>
                 </div>
-                <div>
-                    <h1 className={styles.headerName}>{appName}</h1>
-                    {appDeveloper && (
-                        <span className={styles.headerDeveloper}>
-                            {i18n.t('by {{- developer}}', {
-                                developer: appDeveloper,
-                                context: 'developer of application',
-                            })}
-                        </span>
-                    )}
-                </div>
-                <div>
-                    {installedApp?.launchUrl && (
-                        <a
-                            href={installedApp.launchUrl}
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            <Button className={styles.openLink}>
-                                {i18n.t('Open')}
-                            </Button>
-                        </a>
-                    )}
+                <div
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <div>
+                        <ManageInstalledVersion
+                            installedApp={installedApp}
+                            versions={appHubApp?.versions}
+                            onVersionInstall={onVersionInstall}
+                            onUninstall={onUninstall}
+                        />
+                        {/* {installedApp && appHubApp && (
+                            <div>
+                                <h2 className={styles.sectionHeader}>
+                                    {i18n.t('Additional information')}
+                                </h2>
+                                <Metadata
+                                    installedVersion={installedApp.version}
+                                    versions={versions}
+                                />
+                            </div>
+                        )} */}
+                    </div>
+                    <div style={{ width: '100%' }}>
+                        {installedApp?.launchUrl && (
+                            <a
+                                href={installedApp.launchUrl}
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                <Button
+                                    style={{ width: '100%' }}
+                                    className={styles.openLink}
+                                >
+                                    {i18n.t('Open')}
+                                </Button>
+                            </a>
+                        )}
+                    </div>
                 </div>
             </header>
-            <Divider />
-            <section className={[styles.section, styles.mainSection].join(' ')}>
+            {/* <Divider /> */}
+            {/* <section className={[styles.section, styles.mainSection].join(' ')}>
                 <div>
                     <h2 className={styles.sectionHeader}>
                         {i18n.t('About this app')}
@@ -152,26 +220,13 @@ export const AppDetails = ({
                         </em>
                     )}
                 </div>
-                <div>
-                    <ManageInstalledVersion
-                        installedApp={installedApp}
-                        versions={appHubApp?.versions}
-                        onVersionInstall={onVersionInstall}
-                        onUninstall={onUninstall}
-                    />
-                    {installedApp && appHubApp && (
-                        <div>
-                            <h2 className={styles.sectionHeader}>
-                                {i18n.t('Additional information')}
-                            </h2>
-                            <Metadata
-                                installedVersion={installedApp.version}
-                                versions={versions}
-                            />
-                        </div>
-                    )}
-                </div>
-            </section>
+            </section> */}
+            <iframe
+                width="100%"
+                height="1000px"
+                style={{ border: 0 }}
+                src={`http://localhost:8080/static/app/${appHubApp.id}?compatibleWith=${dhisVersion}`}
+            />
             {screenshots?.length > 0 && (
                 <>
                     <Divider />
